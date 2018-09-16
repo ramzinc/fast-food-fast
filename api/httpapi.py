e116d21835658b10de537faf46c77db54a6b5dba
Arnold Mpiima <ramzinc101@gmail.com>
2018-09-16 13:34:28 +0300

    Update order http method and validate id's
    The update done using the http POST method
    and uses a validator to check type and
    contents of the dictionary.
    
    Delivers #160369427

--- api/httpapi.py
+++ api/httpapi.py
@@ -1,5 +1,7 @@
-from flask import Flask,jsonify
+from flask import Flask,jsonify,request,make_response
+import json
 app = Flask(__name__)
+ORDER_ID_PRESENT_FLAG = False
 orders = {'orders':[{'id': 1,'items': [{'rice':  10000,'matooke':4000,'chickentika':6000}]},
                     {'id': 2,'items': [{'naan':6000,'beans':1000,'posho': 500}]}
     
@@ -15,19 +17,35 @@ def get_index():
 def get_orders():
     return jsonify(orders)
 
-@app.route('/orders',methods=['GET']) 
+@app.route('/orders/',methods=['POST']) 
 def update_order():
-    return 'kdlfa'
+    order = request.get_json()
+    if validate(order):
+        #append the list of orders
+        for x in order['orders']:
+            response = orders['orders'].append(x)
+        ret = json.dumps(orders) #typecast dict to json
+        #Tell The client that the output is json
+        content_type = [('Content-Type','application/json')]
+        return (ret,201,content_type)
+    else:
+        response = ("The Order Was Malformed",400)
+        return response
+    
+#validate if there is an id , items and that the items is of a list type
+def validate(order):
+    for i in order['orders']:
+        if 'id' in i and 'items' in i and isinstance(i['items'],list) and check_id(i['id']):
+            return True
+        else:
+            return False
 
-@app.route('/orders/',methods=['PUT'])
-def put_order():
-    return "Its been put"
-
-@app.route('/orders/',methods=[''])
-def get_specific_order(order):
-    return "Page with order"
-
-@app.route('/orders/',methods=['GET'])
-def update_order_status():
-    return "ckkk"
-
+def check_id(num):
+    for i in orders["orders"]:
+        #import pdb; pdb.set_trace()
+        if num == i["id"]:
+           ORDER_ID_PRESENT_FLAG = True 
+           return False
+        else:
+           continue
+    return True
