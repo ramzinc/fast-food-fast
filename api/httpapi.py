5a5c3d9f8e3a0108f82e7e8cfb67abdc5adcc9d9
Arnold Mpiima <ramzinc101@gmail.com>
2018-09-19 17:14:12 +0300

    Update status using the PATCH verb and get specific Order
    Commit pushes api that updates the order status
    if it is done or not returns a json object.
    The commit includes some helper scripts
    change_status.
    Commit pushes api endpoint to get a specific order.
    This requires prior knowledge of order id.
    
    [Delivers#160369453],[Delivers #160369448]

--- api/httpapi.py
+++ api/httpapi.py
@@ -1,9 +1,9 @@
 from flask import Flask,jsonify,request,make_response
 import json
 app = Flask(__name__)
-ORDER_ID_PRESENT_FLAG = False
-orders = {'orders':[{'id': 1,'items': [{'rice':  10000,'matooke':4000,'chickentika':6000}]},
-                    {'id': 2,'items': [{'naan':6000,'beans':1000,'posho': 500}]}
+#ORDER_ID_PRESENT_FLAG = False
+orders = {'orders':[{'id': 1,'items': [{'rice':  10000,'matooke':4000,'chickentika':6000}],'done':False},
+                    {'id': 2,'items': [{'naan':6000,'beans':1000,'posho': 500}], 'done':False}
     
 ]}
 
@@ -31,21 +31,52 @@ def update_order():
     else:
         response = ("The Order Was Malformed",400)
         return response
-    
+
+
+@app.route('/orders/<int:order_id>',methods=['GET'])
+def get_specific_order(order_id):
+    if check_id_present(order_id):
+        content_type = [('Content-Type','application/json')]
+        ret = json.dumps(orders['orders'][order_id-1])
+        return (ret,200,content_type)
+    else:
+        return '<p>The Order You Have entered Is InValid.</p>'
+
+@app.route('/orders/<int:order_id>',methods=['PATCH'])
+def update_status(order_id):
+   if check_id_present(order_id):
+      #request_order = request.get_json()
+      order_status = orders['orders'][order_id-1]['done']
+      new_order_status = change_status(order_status)
+      orders["orders"][order_id-1]['done'] = new_order_status
+      content_type = [('Content-Type','application/json')]
+      ret = json.dumps(orders['orders'][order_id-1])
+      content_type = [('Content-Type','application/json')]
+      return (ret,200,content_type)
+
+#HELPER SCRIPTS     
+def change_status(order_status):
+    if order_status == False:
+        return True
+    else:
+        return False
+
 #validate if there is an id , items and that the items is of a list type
 def validate(order):
     for i in order['orders']:
-        if 'id' in i and 'items' in i and isinstance(i['items'],list) and check_id(i['id']):
+        if 'id' in i and 'items' in i and isinstance(i['items'],list) and not check_id_present(i['id']):
             return True
         else:
             return False
 
-def check_id(num):
+#
+def check_id_present(num):
     for i in orders["orders"]:
         #import pdb; pdb.set_trace()
         if num == i["id"]:
-           ORDER_ID_PRESENT_FLAG = True 
-           return False
+           #ORDER_ID_PRESENT_FLAG = True 
+           
+           return True
         else:
            continue
-    return True
+    return False
